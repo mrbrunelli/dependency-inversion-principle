0a171f72a3db19c6c9ba0e565b37ad266c28cc4b
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2020-11-12 07:01:10 -0300

    remove useless examples

--- src/no-principle.ts
+++ src/no-principle.ts
@@ -4,19 +4,10 @@ class CepController {
    async consult(url: string): Promise<any> {
     return await axios.get(url)
   }
-
-   async send(url: string, body: any): Promise<any> {
-    return await axios.post(url, body)
-  }
 }
 
 const cep = new CepController()
 
 cep.consult('https://viacep.com.br/ws/87485000/json/')
-  .then(res => console.log(res.data))
-  .catch(error => console.log(error.message))
-
-// Por falta de uma API de CEPs com método Post, resolvi utilizar minha API de Login para exemplificar
-cep.send('https://api-find.herokuapp.com/api/login', { email: 'fake@fake.com.br', senha: 'fake' })
   .then(res => console.log(res.data))
   .catch(error => console.log(error.message))
